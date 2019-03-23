83068939d5df495a662175ec5ded590518b2471a
Shawn Buttrick <shawn.buttrick@clarivate.com>
2019-03-23 19:58:21 -0400

    adding code

---
+++ redline.py
@@ -0,0 +1,11 @@
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+
+def f(x):
+    while True:
+        x*x
+
+if __name__ == '__main__':
+    processes = cpu_count()
+    pool = Pool(processes)
+    pool.map(f, range(processes))
